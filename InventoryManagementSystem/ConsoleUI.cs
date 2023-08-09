cb8934a1bc1737e2edf1fae8f3c69b2c60693019
Aseel-Sabri <aseel.k.sabri@gmail.com>
2023-08-09 13:01:03 +0300

    Display basic operations menu to console
    operations are not implemented yet, only a menu display

---
+++ InventoryManagementSystem/ConsoleUI.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    internal class ConsoleUI : IUserInterface
+    {
+        enum Operation
+        {
+            None = 0,
+            AddProduct = 1,
+            ViewAllProducts = 2,
+            EditProduct = 3,
+            DeleteProduct = 4,
+            SearchProduct = 5,
+            Exit = 6
+        }
+
+        public void Display()
+        {
+            DisplayMenu();
+        }
+
+        void DisplayMenu()
+        {
+            Operation op;
+            do
+            {
+                DisplayOptions();
+                op = ReadUserOption();
+                PerformOperation(op);
+            } while (op != Operation.Exit);
+        }
+
+        void DisplayOptions()
+        {
+            Console.WriteLine("\nChoose Operation\n");
+            Console.WriteLine("1. Add a product");
+            Console.WriteLine("2. View all products");
+            Console.WriteLine("3. Edit a product");
+            Console.WriteLine("4. Delete a product");
+            Console.WriteLine("5. Search for a product");
+            Console.WriteLine("6. Exit");
+        }
+
+        Operation ReadUserOption()
+        {
+            string? input = Console.ReadLine();
+            if (!string.IsNullOrEmpty(input))
+            {
+                if (int.TryParse(input, out int op) && op > (int)Operation.None && op <= (int)Operation.Exit)
+                {
+                    return (Operation)op;
+                }
+            }
+            Console.WriteLine($"\nOperation must be an integer within the range 1 - {(int)Operation.Exit}");
+            return Operation.None;
+        }
+
+        // TODO: Call Operation Methods
+        void PerformOperation(Operation op)
+        {
+            switch (op)
+            {
+                case Operation.None:
+                    return;
+                case Operation.AddProduct:
+                    {
+                        return;
+                    }
+                case Operation.ViewAllProducts:
+                    {
+                        return;
+                    }
+                case Operation.EditProduct:
+                    {
+                        return;
+                    }
+                case Operation.DeleteProduct:
+                    {
+                        return;
+                    }
+                case Operation.SearchProduct:
+                    {
+                        return;
+                    }
+            }
+        }
+    }
+}
--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IUserInterface userInterface = new ConsoleUI();
+            userInterface.Display();
         }
     }
 }
