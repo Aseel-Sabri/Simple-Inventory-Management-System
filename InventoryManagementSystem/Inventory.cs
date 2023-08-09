f8cfeef441837f650ba7c58a30375206d066777d
Aseel-Sabri <aseel.k.sabri@gmail.com>
2023-08-09 10:25:08 +0300

    Create classes: Product & Inventory.

---
+++ InventoryManagementSystem/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    public class Inventory
+    {
+        public List<Product> Products { get; set; }
+        public Inventory()
+        {
+            Products = new List<Product>();
+        }
+
+        public Inventory(List<Product> products)
+        {
+            Products = products;    
+        }
+    }
+}
