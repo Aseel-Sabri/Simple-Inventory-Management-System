013781efe2ce97e882fd3d66266981a95685796f
Aseel-Sabri <aseel.k.sabri@gmail.com>
2023-09-24 23:05:05 +0300

    Add SQL Repository

---
+++ InventoryManagementSystem/AppConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace InventoryManagementSystem;
+
+public static class AppConfig
+{
+    private static IConfiguration? _iconfiguration;
+
+    static AppConfig()
+    {
+        GetAppSettingsFile();
+    }
+
+    private static void GetAppSettingsFile()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        _iconfiguration = builder.Build();
+    }
+
+    public static string? GetConnectionString(string name)
+    {
+        return _iconfiguration.GetConnectionString(name);
+    }
+}
--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            IProductRepository productRepository = new ProductRepository();
+            IProductRepository productRepository = new MsSqlProductRepository(AppConfig.GetConnectionString("Default"));
             IProductServices productServices = new ProductServices(productRepository);
             IUserInterface userInterface = new ConsoleUserInterface(productServices);
             userInterface.Run();
